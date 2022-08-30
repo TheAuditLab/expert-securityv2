71fa8cab8d9e64acd4e9a043ef9e7b896622258a
Dami Sadiq <dami@theauditlab.com>
2022-08-30 15:49:11 +0100

    updating styling on search and news pages

--- entry.php
+++ entry.php
@@ -1,15 +1,18 @@
+<?php
+    $product_hero_bg = get_field("product_hero_bg");
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> 
 <header>
 <div class="title-div">
     <div> 
         <a class="img-title" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
-        <img class="hero-image"src="<?php the_post_thumbnail_url(); ?>">
+        <img class="hero-image"src="<?php the_post_thumbnail_url(); ?>" style="background-image: url('<?php echo $product_hero_bg['url']; ?>')">
     </div>
     <h2><?php the_title(); ?></h2></a>
 
     <?php if ( !is_singular() ) {
         echo '<div class="blog-post">';
-        echo '<a href="' . get_the_permalink() . '">' . get_the_title() .'</a>';
         echo '<a class="read-btn" href="' . get_the_permalink() . '"> READ MORE </a>';
         echo '</div>';
     }?>
--- single.php
+++ single.php
@@ -1,9 +1,6 @@
 <?php get_header(); ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
-<div class="container">
-
-</div>
 <?php endwhile; endif; ?>
 <footer class="footer">
 </footer>
